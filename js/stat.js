3712d4b714ec72fbdfc03c8d93cddc29bd813b4c
rrramble <ru7ar7@gmail.com>
2018-09-02 18:34:00 +0600

    style: conform to linter's messages

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function renderStatistics (ctx, names, times) {
+function renderStatistics(ctx, names, times) {
   var AREA = {
     x: 100,
     y: 10,
@@ -35,7 +35,7 @@ function renderStatistics (ctx, names, times) {
 
   var HEADERS = ['Ура вы победили!', 'Список результатов:'];
   var HEADER_SIZE = 16;
-  var HEADER_FONT =  HEADER_SIZE + 'px "PT Mono"';
+  var HEADER_FONT = HEADER_SIZE + 'px "PT Mono"';
   var HEADER_COLOR = 'black';
   var HEADER_TOP_MARGIN = 20;
   var HEADER_LEFT_MARGIN = 60;
@@ -62,43 +62,43 @@ function renderStatistics (ctx, names, times) {
    * Sub-functions
    */
 
-  function drawCloud (area) {
+  function drawCloud(area) {
     drawRectangle(area);
-  };
+  }
 
-  function drawRectangle (rect) {
+  function drawRectangle(rect) {
     ctx.fillStyle = rect.color;
     ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
-  };
+  }
 
-  function drawStatistics (area, names, times) {
+  function drawStatistics(area, names, times) {
     var maxTime = maxInArray(times);
-    for (var i = 0; i < names.length; i++) {
+    for(var i = 0; i < names.length; i++) {
       var winnerName = names[i];
       var winnerTime = times[i];
 
       var colorStyle = chooseBarColor(winnerName);
       drawWinner(area, i, winnerName, winnerTime, maxTime, colorStyle);
     }
-  };
+  }
 
-  function drawHeaders (area) {
+  function drawHeaders(area) {
     var x = area.x;
-    for (var i = 0; i < HEADERS.length; i++) {
+    for(var i = 0; i < HEADERS.length; i++) {
       var y = area.y + i * HEADER_SIZE;
       var text = HEADERS[i];
       drawText(text, x, y, HEADER_COLOR, HEADER_FONT);
     }
-  };
+  }
 
-  function drawWinner (boundaries, index, textBelow, score, maxScore, colorStyle) {
+  function drawWinner(boundaries, index, textBelow, score, maxScore, colorStyle) {
     var scoreBarArea = {
       width: BAR_WIDTH,
       height: calcBarHeight(score, maxScore, boundaries.height),
-      x: boundaries.x + index * (BAR_WIDTH + BAR_INNER_SPACE),
+      x: boundaries.x + index *(BAR_WIDTH + BAR_INNER_SPACE),
       yBottom: boundaries.y + boundaries.height - TEXT_SIZE,
       color: colorStyle
-    }
+    };
     scoreBarArea.y = scoreBarArea.yBottom - scoreBarArea.height;
 
     drawRectangle(scoreBarArea);
@@ -110,56 +110,56 @@ function renderStatistics (ctx, names, times) {
     var xScore = scoreBarArea.x;
     var yScore = scoreBarArea.y - TEXT_SIZE;
     drawNumber(score, xScore, yScore);
-  };
+  }
 
-  function chooseBarColor (name) {
-      if (name === CURRENT_PLAYER_NAME) {
-        return CURRENT_PLAYER_COLOR_STYLE;
-      } else {
-        return randomColorStyle();
-      }
-  };
+  function chooseBarColor(name) {
+    if(name === CURRENT_PLAYER_NAME) {
+      return CURRENT_PLAYER_COLOR_STYLE;
+    } else {
+      return randomColorStyle();
+    }
+  }
 
-  function drawText (text, x, y, colorStyle, fontStyle) {
+  function drawText(text, x, y, colorStyle, fontStyle) {
     ctx.fillStyle = colorStyle;
     ctx.font = fontStyle;
     ctx.fillText(text, x, y);
-  };
+  }
 
-  function drawNumber (number, x, y) {
+  function drawNumber(number, x, y) {
     var text = Math.round(number).toString();
     drawText(text, x, y, TEXT_COLOR, TEXT_SIZE);
-  };
+  }
 
-  function areaWithoutMargins (area, top, right, bottom, left) {
+  function areaWithoutMargins(area, top, right, bottom, left) {
     var newArea = Object.assign({}, area);
     newArea.x = area.x + left;
     newArea.y = area.y + top;
     newArea.width = area.width - right - left;
     newArea.height = area.height - top - bottom;
     return newArea;
-  };
+  }
 
-  function calcBarHeight (score, maxScore, areaHeight) {
-    if (maxScore === 0) {
+  function calcBarHeight(score, maxScore, areaHeight) {
+    if(maxScore === 0) {
       return 0;
     }
 
-    var result = (areaHeight - TEXT_SIZE - TEXT_SIZE) / maxScore * score;
+    var result =(areaHeight - TEXT_SIZE - TEXT_SIZE) / maxScore * score;
     return Math.floor(result);
-  };
+  }
 
-  function maxInArray (arr) {
+  function maxInArray(arr) {
     return Math.max.apply(null, arr);
-  };
+  }
 
-  function randomColorStyle () {
+  function randomColorStyle() {
     var alpha = randomInRangeFractional(BAR_ALPHA_CHANNEL_MIN, BAR_ALPHA_CHANNEL_MAX).toFixed(2);
     var colorStyle = 'rgba(' + BAR_RED_CHANNEL + ', ' + BAR_GREEN_CHANNEL + ', ' + BAR_BLUE_CHANNEL + ', ' + alpha + ')';
     return colorStyle;
-  };
+  }
 
-  function randomInRangeFractional (from, to) {
-    return Math.random() * (to - from) + from;
-  };
+  function randomInRangeFractional(from, to) {
+    return Math.random() *(to - from) + from;
+  }
 };
