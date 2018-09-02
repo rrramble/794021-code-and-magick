bed6b433a2616a8c29449cebcf55a6e9163e5386
Rustam Arkharov <ru7ar7@gmail.com>
2018-09-02 09:42:33 +0600

    feature: draw score-board in a rectangle shape

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,31 @@
 'use strict';
 
 function renderStatistics (ctx, names, times) {
+  var AREA = {
+    x: 100,
+    y: 10,
+    width: 420,
+    height: 270,
+    color: 'white'
+  };
+
+  var SHADOW = {
+    x: AREA.x + 10,
+    y: AREA.y + 10,
+    width: AREA.width,
+    height: AREA.height,
+    color: 'rgba(0, 0, 0, 0.7)'
+  };
+
+  drawCloud(SHADOW);
+  drawCloud(AREA);
+
+  function drawCloud (area) {
+    drawRectangle(area);
+  };
+
+  function drawRectangle (rect) {
+    ctx.fillStyle = rect.color;
+    ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+  };
 };
